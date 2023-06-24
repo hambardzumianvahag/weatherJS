d958f380ae2965cf5a836bd5c6efa66fefdaa976
unknown <hambardzumianvahag@gmail.com>
2023-06-24 20:50:23 +0400

    update

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ function getValue() {
     let inputVal = document.querySelector('.input').value
     const apiKey = "4d8fb5b93d4af21d66a2948710284366"
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`
+    let date = new Date()
+    let day = date.getDate()
+    let month = date.getMonth() + 1
+    let year = date.getFullYear()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
     let fetchh = fetch(url)
         .then(response => {
             if (response.ok) {
@@ -20,6 +26,7 @@ function getValue() {
             main.innerHTML = `
             <p>Country : ${response.sys.country}</p>
             <p>City: ${response.name}</p>
+            <p>Date: ${day}.0${month}.${year} ${hours}:${minutes}</p>
             <p>Weather: ${response.weather[0].main}</p>
             <p>Temperature: ${response.main.temp.toFixed(0)}°C/${(response.main.temp.toFixed(0) * 9 / 5) + 32}F</p>
             <p>Wind: ${Math.round(response.wind.speed * 2.23694)} mph</p>
